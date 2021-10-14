c8ad603d628f0c57354425b490a2c367d4da1e90
MoraAlex <alex.mora2908@gmail.com>
2021-10-14 17:09:53 -0500

    adding usecase currency test and comments

--- usecase/getPokemonsConcurrency.go
+++ usecase/getPokemonsConcurrency.go
@@ -12,10 +12,14 @@ type pokemonsConcurrency struct {
 	getPokemonsConcurrency getPokemonsConcurrencyService
 }
 
+//NewPokemonsConcurrency takes (service getPokemonsConcurrencyService interface
+// {GetPokemonsConcurrency(t string, items int, ipw int) ([]*entities.Pokemon, error)}) as parameter
+// and returns a new (getPokemonsUseCase struct {Repo pokemonRepo})
 func NewPokemonsConcurrency(service getPokemonsConcurrencyService) pokemonsConcurrency {
 	return pokemonsConcurrency{getPokemonsConcurrency: service}
 }
 
+//GetPokemonsConcurrency: This method takes (t string, items int, ipw int) as parameters and returns ([]*entities.Pokemon, error)
 func (s pokemonsConcurrency) GetPokemonsConcurrency(t string, items int, ipw int) ([]*entities.Pokemon, error) {
 	pokemons, err := s.getPokemonsConcurrency.GetPokemonsConcurrency(t, items, ipw)
 	return pokemons, err
